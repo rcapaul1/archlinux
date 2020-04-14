12d95475ccb7c1221b93dc75258826418abb2448
rcapaul1 <40087283+rcapaul1@users.noreply.github.com>
2020-04-14 16:18:56 +0200

    Create gui.sh

---
+++ gui.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+pacman -Suy
+pacman -S xorg-server xorg-xinit mesa xorg-twm xterm xorg-xclock cinnamon nemo-fileroller gdm bash-completion xf86-video-fbdev
+
+systemctl enable gdm
+systemctl start gdm

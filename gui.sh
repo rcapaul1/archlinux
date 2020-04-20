b340d605a3dbc52717149e687e8c8bb3a73bfc2c
rcapaul1 <40087283+rcapaul1@users.noreply.github.com>
2020-04-20 15:50:29 +0200

    Update gui.sh

--- gui.sh
+++ gui.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 pacman -Suy
-pacman -S xorg-server xorg-xinit mesa xorg-twm xterm xorg-xclock cinnamon nemo-fileroller gdm bash-completion xf86-video-fbdev
+pacman -S xorg-server xorg-xinit mesa xorg-twm xterm xorg-xclock cinnamon nemo-fileroller gdm bash-completion xf86-video-fbdev --noconfirm
 
 systemctl enable gdm
 systemctl start gdm

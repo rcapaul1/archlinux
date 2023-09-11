519eb31bce893bda9e2e795e579ffaab69f09187
rcapaul1 <40087283+rcapaul1@users.noreply.github.com>
2023-09-11 11:04:57 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-pacman -Suy --noconfirm && pacman -Sy openssh vim git wget man docker docker-compose net-tools base-devel --noconfirm
+pacman -Suy --noconfirm && pacman -Sy openssh vim git wget man docker docker-compose net-tools base-devel iperf3 --noconfirm
 echo "[archlinuxfr] SigLevel = Never Server = http://repo.archlinux.fr/$arch" >> /etc/pacman.conf
 pacman -Suy --noconfirm && pacman -Sy base-devel git wget yajl screen tmux ncdu --noconfirm

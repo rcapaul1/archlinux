81f7b7282c29a15442d5a1d973cc8c1d33607844
rcapaul1 <40087283+rcapaul1@users.noreply.github.com>
2026-04-07 10:36:01 +0200

    Update run.sh to include Chaotic AUR setup
    Add Chaotic AUR repository and install keyring.

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 pacman -Suy --noconfirm && pacman -Sy openssh calc vim git wget man docker docker-compose net-tools base-devel iperf3 npm nodejs --noconfirm
-echo "[archlinuxfr] SigLevel = Never Server = http://repo.archlinux.fr/$arch" >> /etc/pacman.conf
+sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign-key 3056513887B78AEB
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+echo "[chaotic-aur] Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
 pacman -Suy --noconfirm && pacman -Sy base-devel git wget yajl screen tmux ncdu --noconfirm
